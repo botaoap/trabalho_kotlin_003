8793da7656beec8e28f858e4634e802015bc7047
botaoap <gabrielbotaoap@gmail.com>
2021-09-04 18:48:40 -0300

    Include Independecy Injection

---
+++ crud_room/app/src/main/java/com/serasa/crud_room/CrudModule.kt
@@ -0,0 +1,9 @@
+package com.serasa.crud_room
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class CrudModule : Application() {
+
+}
--- crud_room/app/src/main/java/com/serasa/crud_room/di/HiltModule.kt
+++ crud_room/app/src/main/java/com/serasa/crud_room/di/HiltModule.kt
@@ -1,4 +1,45 @@
 package com.serasa.crud_room.di
 
+import android.content.Context
+import com.serasa.crud_room.database.AppDatabase
+import com.serasa.crud_room.database.dao.AppointmentDAO
+import com.serasa.crud_room.database.dao.CategoryDAO
+import com.serasa.crud_room.database.dao.DoctorDAO
+import com.serasa.crud_room.database.dao.PatientDAO
+import com.serasa.crud_room.model.Appointment
+import com.serasa.crud_room.model.Patient
+import com.serasa.crud_room.repository.PatientRepository
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
 class HiltModule {
+
+    @Provides
+    fun providesPatient(@ApplicationContext context: Context): PatientDAO {
+        return AppDatabase.getDatabase(context).patientDAO()
+    }
+
+    @Provides
+    fun providesDoctor(@ApplicationContext context: Context): DoctorDAO {
+        return AppDatabase.getDatabase(context).doctorDAO()
+    }
+
+    @Provides
+    fun providesCategory(@ApplicationContext context: Context): CategoryDAO {
+        return AppDatabase.getDatabase(context).categoryDAO()
+    }
+
+    @Provides
+    fun providesAppointment(@ApplicationContext context: Context): AppointmentDAO {
+        return AppDatabase.getDatabase(context).appointmentDAO()
+    }
+
+    @Provides
+    fun providesPatientRepository(patitent: PatientDAO): PatientRepository = PatientRepository(patitent)
+
 }
