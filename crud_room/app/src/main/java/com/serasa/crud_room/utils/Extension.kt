cd7e5633753779d63b44a1397b27817a79e3267c
botaoap <gabrielbotaoap@gmail.com>
2021-09-07 20:02:48 -0300

    HideKeyboard

--- crud_room/app/src/main/java/com/serasa/crud_room/utils/Extension.kt
+++ crud_room/app/src/main/java/com/serasa/crud_room/utils/Extension.kt
@@ -1,5 +1,9 @@
 package com.serasa.crud_room.utils
 
+import android.app.Activity
+import android.content.Context
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 import androidx.annotation.IdRes
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
@@ -10,4 +14,13 @@ fun FragmentActivity.replaceView(fragment: Fragment, @IdRes idContainer: Int = R
     supportFragmentManager.beginTransaction()
         .replace(idContainer, fragment)
         .commitNow()
+}
+
+fun FragmentActivity.hideKeyboard() {
+    hideKeyboard(currentFocus ?: View(this))
+}
+
+fun Context.hideKeyboard(view: View) {
+    val inputMethodManager = getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
+    inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
 }
